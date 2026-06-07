a85d6ce03a1d38fc0f0a28c6d590e1fd554e3550
tondikii <tondikiag30@gmail.com>
2026-06-07 19:09:36 +0700

    change home route label UangKu to Atur Keuangan

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, welcome to Uangku server!';
+    return 'Hello, welcome to Atur Keuangan server!';
   }
 }
